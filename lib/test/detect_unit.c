3b17d92ea1605d4161805fd7c0badd03548948c6
Artyom Belov <avb@natsys-lab.com>
2018-11-08 16:09:17 +0300

    test on `join` w/o `using` or `on` clause

--- lib/test/detect_unit.c
+++ lib/test/detect_unit.c
@@ -299,6 +299,22 @@ Tsqli_create_func(void)
     CU_ASSERT_EQUAL(detect_close(detect), 0);
 }
 
+static void
+Tsqli_join_wo_join_qual(void)
+{
+    struct detect *detect;
+    uint32_t attack_types;
+
+    CU_ASSERT_PTR_NOT_NULL_FATAL(detect = detect_open("sqli"));
+    CU_ASSERT_EQUAL(detect_start(detect), 0);
+    CU_ASSERT_EQUAL(
+      detect_add_data(detect,
+                      STR_LEN_ARGS("SELECT * FROM (SELECT * FROM (SELECT 1) \
+                                    as t JOIN (SELECT 2)b)a"), true), 0);
+  CU_ASSERT_EQUAL(detect_has_attack(detect, &attack_types), 1);
+  CU_ASSERT_EQUAL(detect_stop(detect), 0);
+  CU_ASSERT_EQUAL(detect_close(detect), 0);
+}
 int
 main(void)
 {
@@ -322,6 +338,7 @@ main(void)
         {"func", Tsqli_func},
         {"var_start_with_dollar", Tsqli_var_start_with_dollar},
         {"create_func", Tsqli_create_func},
+        {"join_wo_join_qual", Tsqli_join_wo_join_qual},
         CU_TEST_INFO_NULL
     };
     CU_SuiteInfo suites[] = {
