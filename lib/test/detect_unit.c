a4eb34258db7488ec0cb2a36c68b8673dce2e6e9
Artyom Belov <avb@natsys-lab.com>
2018-11-12 13:15:16 +0300

    test on var start with dollar

--- lib/test/detect_unit.c
+++ lib/test/detect_unit.c
@@ -58,6 +58,22 @@ Tsqli_rce(void)
     CU_ASSERT_EQUAL(detect_close(detect), 0);
 }
 
+static void
+Tsqli_var_start_with_dollar(void)
+{
+    struct detect *detect;
+    uint32_t attack_types;
+
+    CU_ASSERT_PTR_NOT_NULL_FATAL(detect = detect_open("sqli"));
+    CU_ASSERT_EQUAL(detect_start(detect), 0);
+    CU_ASSERT_EQUAL(
+        detect_add_data(detect, STR_LEN_ARGS("1 AND $func(a, 2) = 'ab'"), true),
+        0);
+    CU_ASSERT_EQUAL(detect_has_attack(detect, &attack_types), 1);
+    CU_ASSERT_EQUAL(detect_stop(detect), 0);
+    CU_ASSERT_EQUAL(detect_close(detect), 0);
+}
+
 int
 main(void)
 {
@@ -68,6 +84,7 @@ main(void)
     CU_TestInfo sqli_tests[] = {
         {"simplest", Tsqli_simplest},
         {"rce", Tsqli_rce},
+        {"var_start_with_dollar", Tsqli_var_start_with_dollar},
         CU_TEST_INFO_NULL
     };
     CU_SuiteInfo suites[] = {
