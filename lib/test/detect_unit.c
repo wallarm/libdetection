9bcfa4c043e94f1a42d603e4e4ab26293021a157
Artyom Belov <avb@natsys-lab.com>
2018-11-16 13:10:30 +0300

    test on into outfile

--- lib/test/detect_unit.c
+++ lib/test/detect_unit.c
@@ -299,6 +299,23 @@ Tsqli_create_func(void)
     CU_ASSERT_EQUAL(detect_close(detect), 0);
 }
 
+static void
+Tsqli_into_outfile(void)
+{
+    struct detect *detect;
+    uint32_t attack_types;
+
+    CU_ASSERT_PTR_NOT_NULL_FATAL(detect = detect_open("sqli"));
+    CU_ASSERT_EQUAL(detect_start(detect), 0);
+    CU_ASSERT_EQUAL(
+        detect_add_data(detect,
+                        STR_LEN_ARGS("SELECT 1 FROM table_name INTO OUTFILE 1"),
+                        true), 0);
+    CU_ASSERT_EQUAL(detect_has_attack(detect, &attack_types), 1);
+    CU_ASSERT_EQUAL(detect_stop(detect), 0);
+    CU_ASSERT_EQUAL(detect_close(detect), 0);
+}
+
 int
 main(void)
 {
@@ -322,6 +339,7 @@ main(void)
         {"func", Tsqli_func},
         {"var_start_with_dollar", Tsqli_var_start_with_dollar},
         {"create_func", Tsqli_create_func},
+        {"into_outfile", Tsqli_into_outfile},
         CU_TEST_INFO_NULL
     };
     CU_SuiteInfo suites[] = {
