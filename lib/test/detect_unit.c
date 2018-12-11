6cdad9c50c7c9b35c7b4e88c33edcc686e035796
Artyom Belov <avb@natsys-lab.com>
2018-12-11 15:50:16 +0300

    test on string literal

--- lib/test/detect_unit.c
+++ lib/test/detect_unit.c
@@ -516,6 +516,23 @@ Tsqli_where(void)
     CU_ASSERT_EQUAL(detect_close(detect), 0);
 }
 
+static void
+Tsqli_string(void)
+{
+    struct detect *detect;
+    uint32_t attack_types;
+
+    CU_ASSERT_PTR_NOT_NULL_FATAL(detect = detect_open("sqli"));
+    CU_ASSERT_EQUAL(detect_start(detect), 0);
+    CU_ASSERT_EQUAL(
+        detect_add_data(detect, STR_LEN_ARGS(
+            "SELECT _latin1 'str' COLLATE latin1_german2_ci a, n'str' b, "
+            "x'str' c, _utf8'str' d"), true), 0);
+    CU_ASSERT_EQUAL(detect_has_attack(detect, &attack_types), 1);
+    CU_ASSERT_EQUAL(detect_stop(detect), 0);
+    CU_ASSERT_EQUAL(detect_close(detect), 0);
+}
+
 int
 main(void)
 {
@@ -551,6 +568,7 @@ main(void)
         {"select_alias", Tsqli_select_alias},
         {"drop", Tsqli_drop},
         {"where", Tsqli_where},
+        {"string", Tsqli_string},
         CU_TEST_INFO_NULL
     };
     CU_SuiteInfo suites[] = {
