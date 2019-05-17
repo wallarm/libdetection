d234c6a5dd42d87f4ffdcf3431dca92ad0857682
Artyom Belov <avb@tempesta-tech.com>
2019-05-17 13:50:21 +0300

    add tests on vars

--- lib/test/detect_unit.c
+++ lib/test/detect_unit.c
@@ -520,6 +520,9 @@ Tbash_simplest(void)
         {CSTR_LEN("l's' -a")},
         {CSTR_LEN("l\\s -a")},
         {CSTR_LEN("VAR=VAL ls")},
+        {CSTR_LEN("VAR=VAL l$1s")},
+        {CSTR_LEN("VAR=VAL l$name's'")},
+        {CSTR_LEN("VAR=VAL l${name}s")},
     );
 }
 
